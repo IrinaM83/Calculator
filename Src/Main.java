fdfea9d4944af05800c93ff610d9cfdd2db3046c
Ирина <108092839+IrinaM83@users.noreply.github.com>
2022-06-25 10:17:18 +0300

    Update Main.java

--- Src/Main.java
+++ Src/Main.java
@@ -7,17 +7,18 @@ import java.util.Scanner;
 public class Main {
     static int last = 100;
     static int numbers[] = {1, 4, 5, 9, 10, 50, 100, 500};
-    static String letters[]  = { "I", "IV", "V", "IX", "X", "L", "C", "D"};
+    static String letters[] = {"I", "IV", "V", "IX", "X", "L", "C", "D"};
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-        if(controlOperator(str)){
-            try{
+        if (controlOperator(str)) {
+            try {
                 throw new IOException();
-            } catch (IOException e){
+            } catch (IOException e) {
                 System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
             }
-        }else Main.calc(str);
+        } else Main.calc(str);
 
     }
 
@@ -27,13 +28,13 @@ public class Main {
 
         String[] words = input.split("\\+|-|\\*|/");
         int a = 0, b = 0;
-        if (words.length<2){
-            try{
+        if (words.length < 2) {
+            try {
                 throw new IOException();
-            } catch (IOException e){
+            } catch (IOException e) {
                 System.out.println("throws Exception //т.к. строка не является математической операцией");
             }
-        }else if (isNumeric(words[0]) && isNumeric(words[1])) {
+        } else if (isNumeric(words[0]) && isNumeric(words[1])) {
             a = Integer.parseInt(words[0].trim());
             b = Integer.parseInt(words[1].trim());
             if (a < 1 || a > 10 || b < 1 || b > 10) {
@@ -42,7 +43,7 @@ public class Main {
                 } catch (IOException e) {
                     System.out.println("throws Exception //т.к. числа должны быть от 1 до 10 включительно");
                 }
-            } 
+            } else System.out.println(operations(a, b, op));
         } else {
             try {
                 a = convertRomanToInt(words[0].trim());
@@ -53,15 +54,13 @@ public class Main {
                     } catch (IOException e) {
                         System.out.println("throws Exception //т.к. числа должны быть от 1 до 10 включительно");
                     }
-                }
-                else if (operations(a, b, op) < 0) {
+                } else if (operations(a, b, op) < 1) {
                     try {
                         throw new IOException();
                     } catch (IOException e) {
                         System.out.println("throws Exception //т.к. в римской системе нет отрицательных чисел");
                     }
                 } else {
-                    System.out.println();
                     System.out.println(convertIntegerToRoman(operations(a, b, op)));
                 }
 
@@ -69,107 +68,93 @@ public class Main {
             } catch (Exception e) {
                 System.out.println("throws Exception //т.к. используются одновременно разные системы счисления");
             }
+        }
     }
-        }
 
 
-        static String convertIntegerToRoman ( int number){
-            String romanValue = "";
-            int N = number;
-            while (N > 0) {
-                for (int i = 0; i < numbers.length; i++) {
-                    if (N < numbers[i]) {
-                        N -= numbers[i - 1];
-                        romanValue += letters[i - 1];
-                        break;
-                    }
+    static String convertIntegerToRoman(int number) {
+        String romanValue = "";
+        int N = number;
+        while (N > 0) {
+            for (int i = 0; i < numbers.length; i++) {
+                if (N < numbers[i]) {
+                    N -= numbers[i - 1];
+                    romanValue += letters[i - 1];
+                    break;
                 }
             }
-            return romanValue;
         }
+        return romanValue;
+    }
 
-        static int convertRomanToInt (String romanNumeral){
-            int integerValue = 0;
-            for (int i = 0; i < romanNumeral.length(); i++) {
-                char ch = romanNumeral.charAt(i);
-                int number = letterToNumber(ch);
-                if (number == -1) {
-                    throw new NumberFormatException("Invalid format");
-                }
-                if (last < number) number -= last - 1;
-                integerValue += number;
-                last = number;
-            }
-            return integerValue;
+    static int convertRomanToInt(String romanNumeral) {
+        int integerValue = 0;
+        integerValue = letterToNumber(romanNumeral);
+        return integerValue;
+    }
+
+    static int letterToNumber(String letter) {
+        if (letter.equals("I")) return 1;
+        else if (letter.equals("II")) return 2;
+        else if (letter.equals("III")) return 3;
+        else if (letter.equals("IV")) return 4;
+        else if (letter.equals("V")) return 5;
+        else if (letter.equals("VI")) return 6;
+        else if (letter.equals("VII")) return 7;
+        else if (letter.equals("VIII")) return 8;
+        else if (letter.equals("IX")) return 9;
+        else if (letter.equals("X")) return 10;
+        else return -1;
+    }
+
+    static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
+    }
 
-        static int letterToNumber ( char letter){
-            switch (letter) {
-                case 'I':
-                    return 1;
-                case 'V':
-                    return 5;
-                case 'X':
-                    return 10;
-                case 'L':
-                    return 50;
-                case 'C':
-                    return 100;
-                default:
-                    return -1;
-            }
+    static int operations(int a, int b, char op) {
+        int c = 0;
+        switch (op) {
+            case '+':
+                c = a + b;
+                break;
+            case '-':
+                c = a - b;
+                break;
+            case '*':
+                c = a * b;
+                break;
+            case '/':
+                c = a / b;
+                break;
         }
+        return c;
+    }
 
-        static boolean isNumeric (String str){
-            int number;
+    static char searchOperator(String str) {
+        char op = 0;
+        if (str.indexOf('+', 1) > 0) op = str.charAt(str.indexOf('+', 1));
+        else if (str.indexOf('-', 1) > 0) op = str.charAt(str.indexOf('-', 1));
+        else if (str.indexOf('*', 1) > 0) op = str.charAt(str.indexOf('*', 1));
+        else if (str.indexOf('/', 1) > 0) op = str.charAt(str.indexOf('/', 1));
+        return op;
+    }
 
-            try {
-                number = Integer.parseInt(str);
-                return true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
-
-        static int operations ( int a, int b, char op){
-            int c = 0;
-            switch (op) {
-                case '+':
-                    c = a + b;
-                    break;
-                case '-':
-                    c = a - b;
-                    break;
-                case '*':
-                    c = a * b;
-                    break;
-                case '/':
-                    c = a / b;
-                    break;
-            }
-            return c;
-        }
-
-        static char searchOperator (String str){
-            char op = 0;
-            if (str.indexOf('+', 1) > 0) op = str.charAt(str.indexOf('+', 1));
-            else if (str.indexOf('-', 1) > 0) op = str.charAt(str.indexOf('-', 1));
-            else if (str.indexOf('*', 1) > 0) op = str.charAt(str.indexOf('*', 1));
-            else if (str.indexOf('/', 1) > 0) op = str.charAt(str.indexOf('/', 1));
-            return op;
-        }
-
-        static boolean controlOperator (String str){
-        int count=0;
+    static boolean controlOperator(String str) {
+        int count = 0;
         char[] op = {'+', '-', '*', '/'};
         char[] strCh = str.toCharArray();
-        for (int i =0; i < op.length; i++){
-            for (int j = 0; j < strCh.length; j++){
-                if (op[i] == strCh[j]){
+        for (int i = 0; i < op.length; i++) {
+            for (int j = 0; j < strCh.length; j++) {
+                if (op[i] == strCh[j]) {
                     count++;
                 }
             }
         }
-        return count>1;
-        }
+        return count > 1;
     }
+}
